e0e90f4b6efb4d94f76b53b46cd6e86a3634d5d2
flgusa <arr_1991@mail.ru>
2024-03-16 14:46:02 +0500

    Добавить заказ и хранилище заказов

---
+++ domain/Store.Tests/OrderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_withNullItems_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Order(1, null));
+        }
+
+        [Fact]
+        public void TotalCount_WithEmptyItems_ReturnsZero()
+        {
+            var order = new Order(1, new OrderItem[0]);
+            Assert.Equal(0, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithEmptyItems_ReturnsZero()
+        {
+            var order = new Order(1, new OrderItem[0]);
+            Assert.Equal(0m, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalCount_WithNonEmptyItems_CalcultesTotalCount()
+        {
+            var order = new Order(1, new[]
+            { 
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m),
+            });
+
+            Assert.Equal(3 + 5, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithNonEmptyItems_CalcultesTotalPrice()
+        {
+            var order = new Order(1, new[]
+            {
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m),
+            });
+
+            Assert.Equal(3 * 10m + 5 * 100m, order.TotalPrice);
+        }
+    }
+}
