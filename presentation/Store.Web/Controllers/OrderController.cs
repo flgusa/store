13ca559739b7fcfdb64ae7a7e120665a8105b6f5
flgusa <arr_1991@mail.ru>
2024-03-19 18:48:43 +0500

    Исправить ошибки на уровне представления

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Store.Web.Models;
-using System;
 using System.Linq;
 
 namespace Store.Web.Controllers
@@ -53,7 +52,7 @@ namespace Store.Web.Controllers
             };
         }
 
-        public IActionResult AddItem(int bookId, int count)
+        public IActionResult AddItem(int bookId, int count = 1)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
 
@@ -63,7 +62,7 @@ namespace Store.Web.Controllers
 
             SaveOrderAndCart(order, cart);
 
-            return RedirectToAction("Index", "Book", new { bookId });
+            return RedirectToAction("Index", "Book", new { id = bookId });
         }
 
         [HttpPost]
